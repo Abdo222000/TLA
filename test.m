1af27c78c1eb452ba8206da763277653e5541135
Abdo222000 <abbasebrahiem123@gmail.com>
2022-10-27 14:25:59 +0200

    added CPU dataset

--- test.m
+++ test.m
@@ -1,2 +1,15 @@
-inputdata = csvread('Diabetesdata.csv');
-data = inputdata(:,1)
+inputdata = csvread('CPU_data_class.csv');
+traindata = inputdata(:,1:6);
+traindatagroup = inputdata(:,7);
+
+samplesnum=input('Enter number of test datapoints:');
+    for i=1:samplesnum
+        x1 = input('Enter the MYCT of the test: \n');
+        x2 = input('Enter the MMIN of the test: \n');
+        x3 = input('Enter the MMAX value of the test: \n');
+        x4 = input('Enter the CACHE of the test: \n');
+        x5 = input('Enter the CHMIN of the test: \n');
+        x6 = input('Enter the CHMAX of the test: \n');
+        sample = [x1 x2 x3 x4 x5 x6];            
+      class = knnclassify(sample,traindata,traindatagroup)
+    end
--- testknn.m
+++ testknn.m
@@ -2,7 +2,8 @@
 flag = 0;
 while (flag==0)
     disp('Type 1 to Use Iris dataset')
-    disp('Tyoe 2 to Use Diabetes dataset')
+    disp('Type 2 to Use Diabetes dataset')
+    disp('Type 3 to Use CPU dataset')
     sel=input('Enter The Selection');
     
 if sel == 1
@@ -49,6 +50,22 @@ elseif sel ==2
         x7 = input('Enter the pedi of the test: \n');
         x8 = input('Enter the age of the test: \n');
         sample = [x1 x2 x3 x4 x5 x6 x7 x8];            
+      class = knnclassify(sample,traindata,traindatagroup)
+    end
+elseif sel==3
+    inputdata = csvread('CPU_data_class.csv');
+    traindata = inputdata(:,1:6);
+    traindatagroup = inputdata(:,7);
+
+    samplesnum=input('Enter number of test datapoints:');
+    for i=1:samplesnum
+        x1 = input('Enter the MYCT of the test: \n');
+        x2 = input('Enter the MMIN of the test: \n');
+        x3 = input('Enter the MMAX value of the test: \n');
+        x4 = input('Enter the CACHE of the test: \n');
+        x5 = input('Enter the CHMIN of the test: \n');
+        x6 = input('Enter the CHMAX of the test: \n');
+        sample = [x1 x2 x3 x4 x5 x6];            
       class = knnclassify(sample,traindata,traindatagroup)
     end
 else
